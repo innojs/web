d46562963176d29d567d5536e394b10bcf2baf2d
Mikhail Troshev <mishanga@yandex-team.ru>
2016-08-20 20:49:07 +0300

    сегодняшнее

--- src/events.js
+++ src/events.js
@@ -10,8 +10,11 @@
         url: 'https://api.github.com/repos/innojs/web-events/issues?access_t' + 'oken=' + tok,
         method:'GET',
         success: function(data) {
+            var event;
             for (var i = 0; i < data.length; i++) {
-                events.push(parseEvent(data[i]));
+                event = parseEvent(data[i]);
+                if (event.now) events.unshift(event);
+                else events.push(event);
             }
 
             $('.feed').html(bh.apply(events)) ;
@@ -25,7 +28,11 @@
         result = JSON.parse(eventJSON);
 
         return {
+            now: result.date === '20.08.2016', // FIXME: сделать нормальную логику показа сегодняшнего мероприятия
             block: 'event',
+            mods: {
+                now: result.date === '20.08.2016'
+            },
             content: [
                 { elem: 'title', content: issue.title },
                 { elem: 'date', content: result.date  },
