db04e1538b2324862b4ed3e210199a4e1bc49e3f
Aidar Shaikhiev <muphblu09@gmail.com>
2016-08-20 17:27:07 +0300

    Merge

--- src/events.js
+++ src/events.js
@@ -1,15 +1,32 @@
 //console.log(bh.apply({ block: 'event', elem: 'title' }));
 (function() {
-  var events;
-  var xhr = new XMLHttpRequest();
-
-  xhr.open('GET', 'https://api.github.com/repos/innojs/web-events/issues', false);
-  xhr.send();
-
-  if (xhr.status != 200) {
-    console.log("УВАГА")
-  } else {
-     events = xhr.responseText; 
-  }
-
+	getIssues();
 })()
+
+var ORG = "innojs";
+var projects = "web-events";
+
+function getIssues() {
+	var events=[];
+	$.ajax({
+		url: "https://api.github.com/repos/innojs/web-events/issues",
+		method:"GET",
+		success: function(data) {
+			for (var i = 0; i < data.length; i++) {
+				events.push(parseEvent(data[i]));
+			}
+
+			//here logc to process update ui
+			console.log(events);
+		}
+	});
+}
+
+function parseEvent(issue){
+	var result = issue.body.match(/^(```(.|\n|\r)+```)/g);
+	var eventJSON = result[0].replace(/```/g,"");
+	var result = JSON.parse(eventJSON);
+	return result;
+}
+
+
