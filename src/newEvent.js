8e4dbe419cbf0760ba46a0f3ccdc34e33879fb7d
Mikhail Troshev <mishanga@yandex-team.ru>
2016-08-20 21:44:21 +0300

    fix

--- src/newEvent.js
+++ src/newEvent.js
@@ -32,7 +32,7 @@ $('.header__add-event').on('click', function() {
             description: $('textarea[name=description]').val()
         };
         addEvent(data);
-        $('.feed').remove(form);
+        form.remove();
         return false;
     });
 
