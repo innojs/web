328ddf42186de40835f99347f256145c6d31166c
Misha Anikanov <misha.anikanov@gmail.com>
2016-08-20 15:31:49 +0300

    button

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ require('./layout.styl');
 require('./blocks/feed/feed.styl');
 require('./blocks/event/event.styl');
 require('./blocks/header/header.styl');
+require('./blocks/button/button.styl');
 
 // BH-шаблоны
 require('./blocks/event/event.bh.js');
