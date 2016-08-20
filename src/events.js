e1e3fcdedfeca5a38ec8cc0d0e43f8dce69fdbb0
Shaikhislamov <marat.ravilevich@gmail.com>
2016-08-20 16:44:54 +0300

    подтягиваем данные с гита

--- src/events.js
+++ src/events.js
@@ -1,1 +1,15 @@
 //console.log(bh.apply({ block: 'event', elem: 'title' }));
+(function() {
+  var events;
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'https://api.github.com/repos/innojs/web-events/issues', false);
+  xhr.send();
+
+  if (xhr.status != 200) {
+    console.log("УВАГА")
+  } else {
+     events = xhr.responseText; 
+  }
+
+})()
