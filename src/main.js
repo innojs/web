b872d971abffe1a21d4830e2f330cb56817cbc3c
Mikhail Troshev <mishanga@yandex-team.ru>
2016-08-20 16:11:29 +0300

    допилил кнопку

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ require('./layout.styl');
 require('./blocks/feed/feed.styl');
 require('./blocks/event/event.styl');
 require('./blocks/logo/logo.styl');
-require('./blocks/header/header.styl');
 require('./blocks/button/button.styl');
+require('./blocks/header/header.styl');
 
 // BH-шаблоны
 require('./blocks/event/event.bh.js');
