49d69a2926fc9ba874ed0123d78b9cccad803cc7
Aidar Shaikhiev <muphblu09@gmail.com>
2016-08-21 00:04:02 +0300

    Fix adding events through OAuth from real user

--- src/newEvent.js
+++ src/newEvent.js
@@ -1,16 +1,9 @@
 OAuth.initialize('-Rzipes15nnYRX0CDOrIUEb-4RE');
-var user_token;
-var user_id;
 
 $('.header__add-event').on('click', function() {
-  OAuth.popup('github', { cache: true })
+  OAuth.popup('github', {cache: true})
   .then(function(github) {
-    user_token = github.access_token;
-    return github.get('/user')
-  })
-  .then(function(data) {
-    user_id = data.id;
-    var form = $(bh.apply({
+    var form = $(bh.apply({ 
         block: 'form',
         content: [
             { block: 'input', name: 'title', hint: 'Название события' },
@@ -22,7 +15,7 @@ $('.header__add-event').on('click', function() {
     }));
     $('.feed').prepend(form);
     $('.form__control').submit(function() {
-        var data = {
+        var event = {
             title: $('input[name=title]').val(),
             date: $('input[name=date]').val(),
             location: {
@@ -31,7 +24,8 @@ $('.header__add-event').on('click', function() {
             },
             description: $('textarea[name=description]').val()
         };
-        addEvent(data);
+        var issueJSON = transformEvent(event);
+        github.post("/repos/innojs/web-events/issues", {data: issueJSON});
         form.remove();
         return false;
     });
@@ -41,24 +35,6 @@ $('.header__add-event').on('click', function() {
     console.log('retry');
   });
 });
-
-function addEvent(event){
-  var issue = transformEvent(event);
-  createIssue(issue);
-}
-
-function createIssue(issueJSON) {
-    var tok = '3bb46320a' + String('39fc3687db2c');
-    tok += '0c10411330827e7b781';
-    $.ajax({
-        url: 'https://api.github.com/repos/innojs/web-events/issues?access_t' + 'oken=' + tok,
-        method: 'POST',
-        data: issueJSON,
-        success: function() {
-            console.log('success');
-        }
-    });
-}
 
 function transformEvent(event){
   var title = event.title;
